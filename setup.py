a8ecbe4a1573e5a55079c1c6b2c48fd1ee8ca6c0
SANJITH-KUMAR-20 <sanjithkumar986@gmail.com>
2024-10-16 21:00:12 +0530

    requirements and settup made ready

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
 setup(
     "identity_cluster",
     version="0.1.0",
     packages=find_packages(),
-    install_requires = [],
+    install_requires = required,
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     github_url="https://github.com/phospheneai/pkg-identity-clustering",
