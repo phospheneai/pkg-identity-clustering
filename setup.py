a3f8bb15383f5434733c56b169cd8a2897f103ed
SANJITH-KUMAR-20 <sanjithkumar986@gmail.com>
2024-10-16 20:59:13 +0530

    requirements and settup made ready

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    "identity_cluster",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires = [],
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    github_url="https://github.com/phospheneai/pkg-identity-clustering",
+    author="Sanjith Kumar @ Phospheneai",
+    author_email="sanjith.kumar@phospheneai.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11'
+)
