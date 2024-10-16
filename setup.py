75d0a061b87f136ef253c8357d63c9215aaed0d8
SANJITH-KUMAR-20 <sanjithkumar986@gmail.com>
2024-10-16 21:19:45 +0530

    setup issue solved

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     required = f.read().splitlines()
 setup(
-    "identity_cluster",
+    name="identity_cluster",
     version="0.1.0",
     packages=find_packages(),
     install_requires = required,
