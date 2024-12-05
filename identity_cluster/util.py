3dba41666fa57e957e26765b2883ac25d9dbce15
Sanjith Kumar R <sanjithkumar986@gmail.com>
2024-12-05 14:05:43 +0530

    Update util.py
    Util updated to include 0 padded face crops and save them for further cross video segment clustering

--- identity_cluster/util.py
+++ identity_cluster/util.py
@@ -26,16 +26,22 @@ def get_video_config(clustered_faces : Dict[int,list], identity : int, identity_
     config["path"] = identity_dir
     config["class"] = "unassinged" 
     
-    
+    exact_crop_dir = os.path.join(identity_dir, "exact_crops")
+    if not os.path.exists(exact_crop_dir):
+        os.makedirs(exact_crop_dir)
     bboxes = []
     box = {}
-    for frame, cropped_face, bbox in clustered_faces[identity]:
+    for frame, cropped_face, bbox, exact_crop in clustered_faces[identity]:
         if mask_frames and not config["class"]:
             config["class"] = detect_probable_fakes(mask_frames[frame],bbox) # type: ignore
         face_path = os.path.join(identity_dir,f"{frame}.jpg")
         image_cv = cv2.cvtColor(np.array(cropped_face), cv2.COLOR_BGR2RGB)
         cropped_face = Image.fromarray(image_cv)
+        exact_crop_face_path = os.path.join(exact_crop_dir,f"{frame}.jpg")
+        exact_crop = Image.fromarray(exact_crop)
         cropped_face.save(face_path)
+        exact_crop.save(exact_crop_face_path)
+        exact_crop.save()
         box[frame] = bbox
         with open(os.path.join(identity_dir,f"{frame}.json"),"w") as f:
             json.dump({frame : bbox},f)
